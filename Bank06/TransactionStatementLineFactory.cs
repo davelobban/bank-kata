367c5f96f9c3713d8dc91dfce8b6da8eff5517ec
dave <d2_git@lobban-banff.co.uk>
2019-04-19 14:28:03 +0100

    Refactored out TransactionStatementLineFactory and Statement.

---
+++ Bank06/TransactionStatementLineFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bank06
+{
+    public class TransactionStatementLineFactory
+    {
+        public static TransactionStatementLine GetLine(Transaction transaction)
+        {
+            switch (transaction.Type)
+            {
+                case Transaction.TransType.Deposit:
+                    return new DepositTransactionStatementLine(transaction);
+                case Transaction.TransType.Withdrawal:
+                    return new WithdrawalTransactionStatementLine(transaction);
+                default:
+                    throw new NotSupportedException("Unknown transaction type");
+            }
+
+            
+        }
+    }
+}
